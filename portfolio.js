64bc5e303ffc65e909f831c787d12ef3d7d7543b
Kunalverma001 <161125292+Kunalverma001@users.noreply.github.com>
2024-06-11 19:15:19 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,54 @@
+let percent85 = document.getElementById("percent85");
+let counter = 0;
+setInterval(() => {
+    if(counter == 85){
+        clearInterval
+    }else{
+    counter += 1;
+    percent85.innerHTML = counter + "%"+"<br>"+"HTML";
+    }
+},22)
+
+let percent80 = document.getElementById("percent80");
+let counter1 = 0;
+setInterval(() => {
+    if(counter1 == 80){
+        clearInterval
+    }else{
+    counter1 += 1;
+    percent80.innerHTML = counter + "%"+"<br>"+"CSS";
+    }
+},22)
+
+let percent70 = document.getElementById("percent70");
+let counter2 = 0;
+setInterval(() => {
+    if(counter2 == 70){
+        clearInterval
+    }else{
+    counter2 += 1;
+    percent70.innerHTML = counter2 + "%"+"<br>"+"JavaScript";
+    }
+},27)
+
+let percent60 = document.getElementById("percent60");
+let counter3 = 0;
+setInterval(() => {
+    if(counter3 == 60){
+        clearInterval
+    }else{  
+    counter3 += 1;
+    percent60.innerHTML = counter3 + "%"+"<br>"+"oopsJava";
+    }
+},32)
+
+let percent75 = document.getElementById("percent75");
+let counter4 = 0;
+setInterval(() => {
+    if(counter4 == 75){
+        clearInterval
+    }else{
+    counter4 += 1;
+    percent75.innerHTML = counter4 + "%"+"<br>"+ "MySQl";
+    }
+},26)
